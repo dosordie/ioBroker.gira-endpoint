30d1ba4c443f7557681b509b89da9ee724b10869
dosordie <14365099+dosordie@users.noreply.github.com>
2026-01-20 15:03:33 +0100

    Remove info restart trigger

--- build/main.js
+++ build/main.js
@@ -196,18 +196,6 @@ class GiraEndpointAdapter extends utils.Adapter {
                 },
                 native: {},
             });
-            await this.setObjectNotExistsAsync("info.hsRestart", {
-                type: "state",
-                common: {
-                    name: this.translate("HomeServer restart trigger"),
-                    type: "boolean",
-                    role: "button",
-                    read: true,
-                    write: true,
-                    def: false,
-                },
-                native: {},
-            });
             await this.setStateAsync("info.connection", { val: false, ack: true });
             await this.setObjectNotExistsAsync("command", {
                 type: "channel",
@@ -226,7 +214,6 @@ class GiraEndpointAdapter extends utils.Adapter {
                 },
                 native: {},
             });
-            this.subscribeStates("info.hsRestart");
             this.subscribeStates("command.hsRestart");
             this.log.debug(this.translate("Pre-created info states"));
             await this.setObjectNotExistsAsync("CO@", {
